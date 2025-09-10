ba666ead1fa09efe433dcd6645995eecf31488b2
Angelo Mendes <angelo030825@gmail.com>
2025-09-10 09:10:21 -0300

    Initial Commit

---
+++ MinhaEstante/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MinhaEstante
+//
+//  Created by Ângelo Mendes on 10/09/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
